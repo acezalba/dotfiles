973baacf0ce8e16a26ea9b168f824698ce69b953
Ace A <acezalba+github@slmail.me>
2024-04-16 23:56:16 +0800

    adjusted to local machine settings

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,20 +9,20 @@ alias v='nvim -w ~/.vimlog "$@"'
 alias vi='nvim -w ~/.vimlog "$@"'
 alias vim='nvim -w ~/.vimlog "$@"'
 
-alias zn='vim $NOTES_DIR/$(date +"%Y%m%d%H%M.md")'
+# alias zn='vim $NOTES_DIR/$(date +"%Y%m%d%H%M.md")'
 
-alias ta='tmux attach -t'
+# alias ta='tmux attach -t'
 
 alias l='exa -lah'
 alias ls=exa
 alias sl=exa
 alias c='clear'
 alias s='source ~/.zshrc'
-alias h=heroku
+# alias h=heroku
 alias jj='pbpaste | jsonpp | pbcopy'
 alias rm=trash
 alias trim="awk '{\$1=\$1;print}'"
-alias notes="cd $NOTES_DIR && nvim 00\ HOME.md"
+# alias notes="cd $NOTES_DIR && nvim 00\ HOME.md"
 
 # GIT ALIASES -----------------------------------------------------------------
 alias gc='git commit'
@@ -98,8 +98,6 @@ mff ()
     gco $curr_branch
 }
 
-
-
 JOBFILE="$DOTFILES/job-specific.sh"
 if [ -f "$JOBFILE" ]; then
     source "$JOBFILE"
@@ -122,7 +120,6 @@ dreset () {
     docker system prune -a
 }
 
-
 extract-audio-and-video () {
     ffmpeg -i "$1" -c:a copy obs-audio.aac
     ffmpeg -i "$1" -c:v copy obs-video.mp4
@@ -135,9 +132,6 @@ hs () {
  curl https://httpstat.us/$1
 }
 
-# alias dp='displayplacer "id:83F2F7DC-590D-6294-B7FB-521754A2A693 res:3840x2160 hz:60 color_depth:8 scaling:off origin:(0,0) degree:0" "id:BD0804E4-6EAA-1C8D-1CFB-D6B734DE10A5 res:3840x2160 hz:60 color_depth:8 scaling:off origin:(3840,0) degree:0"'
-# alias mirror-displays='displayplacer "id:C3F5FA73-E883-4B6D-88B3-DA6D6A8192B3+7ECC0B33-A07B-46A6-AFB8-565FEFE68216 res:3840x2160 hz:60 color_depth:8 scaling:off origin:(0,0) degree:0"'
-
 copy-line () {
   rg --line-number "${1:-.}" | sk --delimiter ':' --preview 'bat --color=always --highlight-line {2} {1}' | awk -F ':' '{print $3}' | sed 's/^\s+//' | pbcopy
 }
@@ -146,9 +140,3 @@ open-at-line () {
   vim $(rg --line-number "${1:-.}" | sk --delimiter ':' --preview 'bat --color=always --highlight-line {2} {1}' | awk -F ':' '{print "+"$2" "$1}')
 }
 
-# alias ledger='ledger -f "$(find $NOTES_DIR -name transactions.ledger)"'
-alias lg='ledger -f "$(find $NOTES_DIR -name 2024.ledger)"'
-
-alias yip='yarn install --pure-lockfile'
-
-alias dark="$DOTFILES/bin/toggle-terminal-dark-mode.sh"
