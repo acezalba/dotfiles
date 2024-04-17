637e2528727cf4da921bbb13cc0bb1a2eacc3001
Ace A <acezalba+github@slmail.me>
2024-04-17 20:39:16 +0800

    deleted line supposed to be in rc

--- scripts/install-nvm.sh
+++ scripts/install-nvm.sh
@@ -10,8 +10,3 @@ export NVM_DIR="$HOME/src/.nvm" && (
   cd "$NVM_DIR"
   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 ) && \. "$NVM_DIR/nvm.sh"
-
-# plug nvm to zsh
-export NVM_DIR="$HOME/src/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
