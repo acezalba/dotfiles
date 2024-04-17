a8a2d2b31119f8f792485d7f129773cfb093a832
Ace A <acezalba+github@slmail.me>
2024-04-17 20:40:36 +0800

    added nvm path

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -67,6 +67,12 @@ export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
+# 2024-04-17, 08:39:43 PM - nvm path
+# register nvm dir to zsh
+export NVM_DIR="$HOME/src/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # ===== OMZ =================================== #
 
 # OMZ settings
