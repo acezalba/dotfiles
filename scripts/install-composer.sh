f0924330352223816b57b9cea1cbff449de73f3c
Ace A <acezalba+github@slmail.me>
2024-04-18 12:55:51 +0800

    added composer install script

---
+++ scripts/install-composer.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Script taken from
+# https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
+
+# Modified to make composer phar global
+
+function download_composer() {
+    EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+    if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+    then
+        >&2 echo 'ERROR: Invalid installer checksum'
+        rm composer-setup.php
+        exit 1
+    fi
+
+    php composer-setup.php --quiet
+    RESULT=$?
+    rm composer-setup.php
+}
+
+function make_composer_global() {
+    sudo mv composer.phar /usr/local/bin/composer
+}
+
+download_composer
+make_composer_global
