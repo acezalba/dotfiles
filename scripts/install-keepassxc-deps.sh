3abf823904d4eed6e5e027dcab43e8eb2260b87d
Ace A <acezalba+github@slmail.me>
2024-04-18 12:50:48 +0800

    edited script

--- scripts/install-keepassxc-deps.sh
+++ scripts/install-keepassxc-deps.sh
@@ -3,7 +3,7 @@
 # install deps needed for
 # compiling keepassxc
 
-sudo apt install qtbase5-dev qtbase5-private-dev qttools5-dev qttools5-dev-tools \\
-    libqt5svg5-dev libargon2-dev libminizip-dev libbotan-2-dev libqrencode-dev \\
-    libkeyutils-dev zlib1g-dev libreadline-dev libpcsclite-dev libusb-1.0-0-dev \\
+sudo apt install qtbase5-dev qtbase5-private-dev qttools5-dev qttools5-dev-tools \
+    libqt5svg5-dev libargon2-dev libminizip-dev libbotan-2-dev libqrencode-dev \
+    libkeyutils-dev zlib1g-dev libreadline-dev libpcsclite-dev libusb-1.0-0-dev \
     libxi-dev libxtst-dev  libqt5x11extras5-dev
