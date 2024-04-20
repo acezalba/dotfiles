9b1185a72e27afae3ac7d56befd886547db8f739
Ace A <acezalba+github@slmail.me>
2024-04-20 22:35:05 +0800

    updated dependencies on calibre script

--- scripts/install-calibre.sh
+++ scripts/install-calibre.sh
@@ -5,6 +5,7 @@
 
 # SRC:
 # https://manual.calibre-ebook.com/develop.html
+# https://github.com/kovidgoyal/calibre/blob/master/bypy/README.rst
 # 
 
 repo="https://github.com/acezalba/calibre.git"
@@ -17,12 +18,21 @@ dir="/home/ghost/src/"
 #     exit 1
 # fi
 
+# Calibre requires a compiled version of podofo. v0.10. refer to repo.
+# todo: compile and expose podofo to linux lib path.
 install_dependencies(){
-    sudo apt install -y uchardet hunspell
+    sudo apt install -y libuchardet-dev libhunspell-dev libpodofo-dev libstemmer-dev libpodofo-utils libpodofo0.9.7    
 }
 
 clone_repo(){
     git clone $repo $dir/calibre
+}
+
+setup_calibre(){
+    python3 setup.py bootstrap
+    python3 setup.py build_dep linux
+    python3 setup.py build_dep linux-arm64
+    python3 setup.py develop
 }
 
 install_dependencies
