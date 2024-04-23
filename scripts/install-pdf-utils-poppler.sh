336181a02a6a2f4528e6cbf03ee19dca71110f27
Ace A <acezalba+github@slmail.me>
2024-04-23 12:45:11 +0800

    added script for installing poppler/pdf to image utils

---
+++ scripts/install-pdf-utils-poppler.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+
+# 2024-04-23, 12:41:02 PM
+# Use poppler utils to convert pdf to images. so much easier.
+
+sudo apt install poppler-utils
+
+# Package: poppler-utils
+# Architecture: amd64
+# Version: 22.02.0-2ubuntu0.3
+# Multi-Arch: foreign
+# Priority: optional
+# Section: utils
+# Source: poppler
+# Origin: Ubuntu
+# Maintainer: Ubuntu Developers <ubuntu-devel-discuss@lists.ubuntu.com>
+# Original-Maintainer: Debian freedesktop.org maintainers <pkg-freedesktop-maintainers@lists.alioth.debian.org>
+# Bugs: https://bugs.launchpad.net/ubuntu/+filebug
+# Installed-Size: 680
+# Provides: pdftohtml, xpdf-utils
+# Depends: libpoppler118 (= 22.02.0-2ubuntu0.3), libc6 (>= 2.34), libcairo2 (>= 1.12.0), libfreetype6 (>= 2.2.1), liblcms2-2 (>= 2.2+git20110628), libstdc++6 (>= 5.2)
+# Conflicts: pdftohtml
+# Breaks: xpdf-common, xpdf-utils (<< 1:0)
+# Replaces: pdftohtml, xpdf-reader, xpdf-utils (<< 3.02-2~)
+# Filename: pool/main/p/poppler/poppler-utils_22.02.0-2ubuntu0.3_amd64.deb
+# Size: 186058
+# MD5sum: d8f7a83f817570518ca542adc8578987
+# SHA1: 3daf1c56cc118c1902cb1f1ab08a46f27101cd7c
+# SHA256: 215650eb127fb91372753dde3820bc6717302119137260c2535bafe41037d725
+# SHA512: 06999c740b76ff3a96a4a4eaa236130653b09d524305f757bbd951c9194a3a22176717eb44b9c0b1e552b797eef4b0a467ab0f205229d4a438a756b756440868
+# Homepage: https://poppler.freedesktop.org/
+# Description-en: PDF utilities (based on Poppler)
+#  Poppler is a PDF rendering library based on Xpdf PDF viewer.
+#  .
+#  This package contains command line utilities (based on Poppler) for getting
+#  information of PDF documents, convert them to other formats, or manipulate
+#  them:
+#   * pdfdetach -- lists or extracts embedded files (attachments)
+#   * pdffonts -- font analyzer
+#   * pdfimages -- image extractor
+#   * pdfinfo -- document information
+#   * pdfseparate -- page extraction tool
+#   * pdfsig -- verifies digital signatures
+#   * pdftocairo -- PDF to PNG/JPEG/PDF/PS/EPS/SVG converter using Cairo
+#   * pdftohtml -- PDF to HTML converter
+#   * pdftoppm -- PDF to PPM/PNG/JPEG image converter
+#   * pdftops -- PDF to PostScript (PS) converter
+#   * pdftotext -- text extraction
+#   * pdfunite -- document merging tool
+# Description-md5: c82ab1c01fb88c7aab560b4a6154c69f
+# Task: print-server, ubuntu-desktop-minimal, ubuntu-desktop, ubuntu-desktop-raspi, kubuntu-desktop, xubuntu-core, xubuntu-desktop, lubuntu-desktop, ubuntustudio-desktop-core, ubuntustudio-desktop, ubuntukylin-desktop, ubuntu-mate-core, ubuntu-mate-desktop, ubuntu-budgie-desktop, ubuntu-budgie-desktop-raspi
+
+# Package: poppler-utils
+# Architecture: amd64
+# Version: 22.02.0-2
+# Multi-Arch: foreign
+# Priority: optional
+# Section: utils
+# Source: poppler
+# Origin: Ubuntu
+# Maintainer: Ubuntu Developers <ubuntu-devel-discuss@lists.ubuntu.com>
+# Original-Maintainer: Debian freedesktop.org maintainers <pkg-freedesktop-maintainers@lists.alioth.debian.org>
+# Bugs: https://bugs.launchpad.net/ubuntu/+filebug
+# Installed-Size: 680
+# Provides: pdftohtml, xpdf-utils
+# Depends: libpoppler118 (= 22.02.0-2), libc6 (>= 2.34), libcairo2 (>= 1.12.0), libfreetype6 (>= 2.2.1), liblcms2-2 (>= 2.2+git20110628), libstdc++6 (>= 5.2)
+# Conflicts: pdftohtml
+# Breaks: xpdf-common, xpdf-utils (<< 1:0)
+# Replaces: pdftohtml, xpdf-reader, xpdf-utils (<< 3.02-2~)
+# Filename: pool/main/p/poppler/poppler-utils_22.02.0-2_amd64.deb
+# Size: 185986
+# MD5sum: 070df6db4ba2f125b71d16ef7f6d1a46
+# SHA1: d60b5434bec9a52b5a4cb0459f9e3efd87581a06
+# SHA256: 24325c2a03d15b8e9780f9a4c467a4513e25530566b2c4a6feecc6e05507d9c1
+# SHA512: 5b0f868e74f48e0e57b2c9504a6416284fb35a52ce3140fd1e53100e51e8ffae5098f749b36728845e19321e96e7b50939b020e3f2315836501e13f988a3e558
+# Homepage: https://poppler.freedesktop.org/
+# Description-en: PDF utilities (based on Poppler)
+#  Poppler is a PDF rendering library based on Xpdf PDF viewer.
+#  .
+#  This package contains command line utilities (based on Poppler) for getting
+#  information of PDF documents, convert them to other formats, or manipulate
+#  them:
+#   * pdfdetach -- lists or extracts embedded files (attachments)
+#   * pdffonts -- font analyzer
+#   * pdfimages -- image extractor
+#   * pdfinfo -- document information
+#   * pdfseparate -- page extraction tool
+#   * pdfsig -- verifies digital signatures
+#   * pdftocairo -- PDF to PNG/JPEG/PDF/PS/EPS/SVG converter using Cairo
+#   * pdftohtml -- PDF to HTML converter
+#   * pdftoppm -- PDF to PPM/PNG/JPEG image converter
+#   * pdftops -- PDF to PostScript (PS) converter
+#   * pdftotext -- text extraction
+#   * pdfunite -- document merging tool
+# Description-md5: c82ab1c01fb88c7aab560b4a6154c69f
+# Task: print-server, ubuntu-desktop-minimal, ubuntu-desktop, ubuntu-desktop-raspi, kubuntu-desktop, xubuntu-core, xubuntu-desktop, lubuntu-desktop, ubuntustudio-desktop-core, ubuntustudio-desktop, ubuntukylin-desktop, ubuntu-mate-core, ubuntu-mate-desktop, ubuntu-budgie-desktop, ubuntu-budgie-desktop-raspi
