90b6446240bf76a6af5c479a18c7d074befb828d
Ace A <acezalba+github@slmail.me>
2024-04-26 08:22:37 +0800

    added script for chromium install

---
+++ scripts/install-chromium-ppa.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install latest chromium release from ppa
+# SRC: 
+# 2024-04-26, 08:01:24 AM
+
+sudo add-apt-repository ppa:savoury1/pipewire
+sudo add-apt-repository ppa:savoury1/chromium
+sudo apt-get update
+sudo apt-get install chromium-browser -y
