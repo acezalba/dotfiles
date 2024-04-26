77296064ea03da757ba61e467ee4cc8dc91d9c37
Ace A <acezalba+github@slmail.me>
2024-04-26 08:27:11 +0800

    added todo for deprec apt-key line

--- scripts/install-chrome-ppa.sh
+++ scripts/install-chrome-ppa.sh
@@ -4,6 +4,7 @@
 # 2024-04-26, 08:23:53 AM
 # SRC: https://www.ubuntuupdates.org/ppa/google_chrome
 
+# TODO: apt-key deprecated. substitute line with alternative.
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update 
