f5857965fd65aeed0b88aa9fc04fc748af65f192
Ace A <acezalba+github@slmail.me>
2024-04-26 09:39:24 +0800

    added install script for github cli

---
+++ scripts/install-github-cli.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install script for github cli ppa
+# 2024-04-26, 09:39:06 AM
+# SRC: https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
