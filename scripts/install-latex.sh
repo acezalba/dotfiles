82b3dcb1b57b508ab2aee0f9701c4a8a96411caf
Ace Z. Alba <acezalba+github@slmail.me>
2024-10-05 16:23:53 +0800

    added a latex automated installer

---
+++ scripts/install-latex.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Installs latex using an automated script
+
+cd $HOME
+wget https://github.com/scottkosty/install-tl-ubuntu/raw/master/install-tl-ubuntu
+chmod +x $HOME/install-tl-ubuntu
+sudo $HOME/install-tl-ubuntu
